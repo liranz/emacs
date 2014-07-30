59ead1285cef23d990d57684d5c2a153bf9376f0
Liran Zvibel <liran@weka.io>
2014-07-30 22:17:37 +0300

    Add D mode

---
+++ parts/033-d-mode.el
@@ -0,0 +1,25 @@
+(add-to-list 'load-path (in-modes-d "d-mode"))
+(add-to-list 'load-path (in-modes-d "dcd-mode"))
+
+(require 'd-mode)
+(autoload 'd-mode "d-mode" "Major mode for editing D code." t)
+(add-to-list 'auto-mode-alist '("\\.d[i]?\\'" . d-mode))
+
+;(add-to-list 'load-path "path_to_ac-dcd.el")
+(require 'ac-dcd)
+(add-to-list 'ac-modes 'd-mode)
+
+(add-hook 'd-mode-hook
+		  '(lambda () "set up ac-dcd"
+			 (ac-dcd-maybe-start-server)
+			 (add-to-list 'ac-sources 'ac-source-dcd)))
+
+(define-key d-mode-map (kbd "C-c ?") 'ac-dcd-show-ddoc-with-buffer)
+(define-key d-mode-map (kbd "C-c .") 'ac-dcd-goto-definition)
+(define-key d-mode-map (kbd "C-c ,") 'ac-dcd-goto-def-pop-marker)
+
+(when (featurep 'popwin)
+  (add-to-list 'popwin:special-display-config
+			   `(,ac-dcd-error-buffer-name :noselect t))
+  (add-to-list 'popwin:special-display-config
+				 `(,ac-dcd-document-buffer-name :position right :width 80)))
